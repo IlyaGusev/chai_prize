ccba6f7a9b87e01fe76dd6fdd6c118ff685d10f7
Ilya Gusev <phoenixilya@gmail.com>
2023-09-21 15:57:50 +0000

    Format fixes

--- chai_prize/formatter.py
+++ chai_prize/formatter.py
@@ -7,3 +7,11 @@ class RPRFormatter(PromptFormatter):
     bot_template = "<s>bot\n{message}</s>"
     user_template = "<s>user\n{message}</s>"
     response_template = "<s>bot\n"
+
+
+class CustomFormatterV2(PromptFormatter):
+    memory_template = "<s>system: You are {bot_name}. {memory}</s>"
+    prompt_template = "<s>prompt: {prompt}</s>"
+    bot_template = "<s>{bot_name}: {message}</s>"
+    user_template = "<s>User: {message}</s>"
+    response_template = "<s>{bot_name}"
