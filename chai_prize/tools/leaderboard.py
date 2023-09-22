0fb5191fb60390b99377cfb61d22d9f20d4a8423
Ilya Gusev <phoenixilya@gmail.com>
2023-09-22 13:10:20 +0000

    Tools fixes

--- chai_prize/tools/leaderboard.py
+++ chai_prize/tools/leaderboard.py
@@ -1,5 +1,6 @@
 import fire
 import pandas as pd
+from tqdm import tqdm
 import chai_guanaco as chai
 from chai_guanaco.login_cli import auto_authenticate
 from chai_guanaco.metrics import get_submission_metrics
@@ -9,7 +10,7 @@ from chai_guanaco.metrics import get_submission_metrics
 def get_my_submissions(developer_key):
     submission_ids = chai.get_my_submissions()
     leaderboard = []
-    for submission_id in submission_ids:
+    for submission_id in tqdm(submission_ids):
         metrics = get_submission_metrics(submission_id, developer_key)
         leaderboard.append({'submission_id': submission_id, **metrics})
     return pd.DataFrame(leaderboard)
@@ -19,6 +20,7 @@ def main(min_feedback_count: int = 100):
     df = get_my_submissions()
     pd.set_option('display.max_columns', 100)
     df = df[df["total_feedback_count"] > min_feedback_count]
+    df = df.drop(["thumbs_up_ratio_se", "user_engagement_se"], axis=1)
     print(df)
 
 
--- chai_prize/tools/submit.py
+++ chai_prize/tools/submit.py
@@ -1,29 +1,36 @@
 import json
+import fire
 import chai_guanaco as chai
 from chai_prize.formatter import CustomFormatterV2
 
-model_url = "IlyaGusev/rolecuna_13b_v4"
 
-generation_params = {
-    "frequency_penalty": 1.2,
-    "presence_penalty": 0.0,
-    "temperature": 1.0,
-    "top_p": 0.9,
-    "top_k": 50
-}
+def submit(model_url):
+    generation_params = {
+        "frequency_penalty": 1.2,
+        "presence_penalty": 0.0,
+        "temperature": 1.0,
+        "top_p": 0.9,
+        "top_k": 50
+    }
 
-submission_parameters = {
-    "model_repo": model_url,
-    "generation_params": generation_params,
-    "model_name": "rolecuna_13b_v4",
-    "formatter": CustomFormatterV2()
-}
-
-submitter = chai.ModelSubmitter()
-submission_id = submitter.submit(submission_parameters)
-with open(f"submissions/{submission_id}.json", "w") as w:
-    json.dump({
-        "model_url": model_url,
+    model_name = model_url.split("/")[-1]
+    submission_parameters = {
+        "model_repo": model_url,
         "generation_params": generation_params,
-        "submission_id": submission_id
-    }, w, indent=4, ensure_ascii=False)
+        "model_name": model_name,
+        "formatter": CustomFormatterV2()
+    }
+
+    submitter = chai.ModelSubmitter()
+    submission_id = submitter.submit(submission_parameters)
+    with open(f"submissions/{submission_id}.json", "w") as w:
+        json.dump({
+            "model_url": model_url,
+            "model_name": model_name,
+            "generation_params": generation_params,
+            "submission_id": submission_id
+        }, w, indent=4, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    fire.Fire(submit)
