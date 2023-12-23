3b8fc6c9dee09bcde816fd28e5db171748c72f8c
Ilya Gusev <phoenixilya@gmail.com>
2023-12-23 23:59:19 +0000

    Fix feedback script

--- chai_prize/tools/feedback.py
+++ chai_prize/tools/feedback.py
@@ -2,6 +2,11 @@ import fire
 import chaiverse as chai
 
 
+def count_user_messages(conversation):
+    if "Anonymous user:" in conversation:
+        return conversation.count("Anonymous user:")
+    return conversation.count("\nXXXXXXXXXXXXXX:")
+
 def show_feedbacks(submission_id: str, public_only: bool = False):
     model_feedback = chai.get_feedback(submission_id)
     df = model_feedback.df
@@ -11,7 +16,8 @@ def show_feedbacks(submission_id: str, public_only: bool = False):
     df["conv_length"] = df["conversation"].apply(lambda x: len(x))
     df["deleted_count"] = df["conversation"].apply(lambda x: x.count("deleted"))
     df["message_count"] = df["conversation"].apply(lambda x: x.count(":"))
-    df["user_message_count"] = df["conversation"].apply(lambda x: x.count("Anonymous user"))
+    df["feedback"] = df["feedback"].apply(lambda x: x[:50])
+    df["user_message_count"] = df["conversation"].apply(count_user_messages)
     df = df.drop(["conversation"], axis=1)
     print(df.to_string())
 
